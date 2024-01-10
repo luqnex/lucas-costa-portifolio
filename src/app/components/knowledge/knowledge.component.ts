757f762a9099f354469f2075b6ea3f6b04b6ac4f
Lucas Costa <lucas.costa4002@gmail.com>
2024-01-10 16:56:00 -0300

    feat: adjust in responsive layout

--- src/app/components/knowledge/knowledge.component.ts
+++ src/app/components/knowledge/knowledge.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-knowledge',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './knowledge.component.html',
-  styleUrl: './knowledge.component.scss'
+  styleUrl: './knowledge.component.scss',
 })
 export class KnowledgeComponent {
+  description: string = '';
 
+  handleSetDescription(type: string): void {
+    switch (type) {
+      case 'html':
+        this.description = 'o HTML é ...';
+        break;
+
+      default:
+        this.description = '';
+    }
+
+    console.log(this.description);
+  }
 }
