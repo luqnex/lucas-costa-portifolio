4840d52d8d33a8a5e69fa0bfb3a6d68cc1e17857
Lucas Costa <lucas.costa4002@gmail.com>
2024-01-14 22:40:10 -0300

    feat: :sparkles: adjust in style of projects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { bootstrapGithub } from '@ng-icons/bootstrap-icons';
+
 import { ButtonComponent } from '../button/button.component';
+import { IRepositoriesData } from '../../interfaces/repositories';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, CommonModule, NgIconComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
+  viewProviders: [provideIcons({ bootstrapGithub })],
 })
 export class ProjectsComponent {
   buttonText: string = 'Ver mais';
   linkToGithub: string = 'https://github.com/luqnex';
+  repositories: IRepositoriesData[] = [];
+
+  showDetailsImageOne: boolean = false;
+  showDetailsImageTwo: boolean = false;
+
+  handleMouseEvent(img: string) {
+    switch (img) {
+      case 'img-one':
+        this.showDetailsImageOne = !this.showDetailsImageOne;
+        break;
+
+      case 'img-two':
+        this.showDetailsImageTwo = !this.showDetailsImageTwo;
+        break;
+    }
+  }
 }
---
+++ src/app/interfaces/repositories.ts
@@ -0,0 +1,6 @@
+export interface IRepositoriesData {
+  id: number;
+  name: string;
+  description: string;
+  html_url: string;
+}
